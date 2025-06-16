b0124fdccdadc36680f7b8025b4a7f1a17111b07
iskandarov1 <iskandarovsanjarbek1327@gmail.com>
2025-06-16 16:49:39 +0500

    Initial commit

---
+++ DT_HR.Domain/Core/Events/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace DT_HR.Domain.Core.Events
+{
+    /// <summary>
+    /// Represents the interface for an event that is raised within the domain.
+    /// </summary>
+    public interface IDomainEvent : INotification
+    {
+    }
+}
